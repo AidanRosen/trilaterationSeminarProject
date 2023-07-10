5c283998295f2e858df32086902879c204a85882
AidanRosen <43625295+AidanRosen@users.noreply.github.com>
2023-07-09 21:26:22 -0700

    Update trilateration.py

--- algorithms/trilateration.py
+++ algorithms/trilateration.py
@@ -1,30 +1,8 @@
 """
-Author	: Muhammad Arifin
-Date	: 7th May, 2020
-Subject	: Implementation of trilateration calculation
+Author	: Aidan Rosen
 
-Licensing		: This program is licensed under MIT License. 
-				  MIT License
-
-Copyright (c) [2020] [Muhammad Arifin]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+Much of this code came from Muhammad Arifin's trilateration code, also found on Github.
+This version is heavily modified though, and the dbClass.py was built by the CSE 191 IoT team in 2023.
 """
 
 import os
